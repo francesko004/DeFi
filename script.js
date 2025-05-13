8064539e27c4e0abc03c23d186ebc6f3af536fcf
francesko004 <franc3sko@proton.me>
2025-05-13 09:02:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -80,13 +80,13 @@ backToTop.addEventListener('click', () => {
 // Particles.js Initialization
 particlesJS('particles', {
     particles: {
-        number: { value: 80, density: { enable: true, value_area: 800 } },
+        number: { value: 100, density: { enable: true, value_area: 800 } },
         color: { value: '#3ad8ff' },
         shape: { type: 'circle' },
         opacity: { value: 0.5, random: true },
         size: { value: 3, random: true },
         line_linked: { enable: true, distance: 150, color: '#8a4af3', opacity: 0.4, width: 1 },
-        move: { enable: true, speed: 2, direction: 'none', random: false }
+        move: { enable: true, speed: 3, direction: 'none', random: false }
     },
     interactivity: {
         detect_on: 'canvas',
